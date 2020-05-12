ca8eb45625cd607fe3f7e2ca0d4a074f25e25dc8
Filip Jerga <jerga99@gmail.comm>
2020-05-12 11:34:18 +0200

    3 Init DB

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 const express = require('express');
 const server = express();
+const config = require('./config/dev');
+
+const mongoose = require('mongoose');
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  if (err) { console.error(err); }
+  else {
+    console.log('Connected to DB!');
+  }
+})
 
 server.use('/api/v1/portfolios', require('./routes/portfolios'));
 
