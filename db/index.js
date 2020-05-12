611e1f458dce1e161bf272efd2d05156be77b01a
Filip Jerga <jerga99@gmail.comm>
2020-05-12 11:44:02 +0200

    4 Structural Changes in DB

---
+++ db/index.js
@@ -0,0 +1,17 @@
+
+const config = require('../config/dev');
+const mongoose = require('mongoose');
+
+exports.connect = () => {
+  return mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }, (err) => {
+    if (err) { console.error(err); }
+    else {
+      console.log('Connected to DB!');
+    }
+  })
+}
--- index.js
+++ index.js
@@ -2,24 +2,18 @@
 
 const express = require('express');
 const server = express();
-const config = require('./config/dev');
 
-const mongoose = require('mongoose');
-mongoose.connect(config.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}, (err) => {
-  if (err) { console.error(err); }
-  else {
-    console.log('Connected to DB!');
-  }
-})
+async function runServer() {
+  await require('./db').connect();
 
-server.use('/api/v1/portfolios', require('./routes/portfolios'));
+  server.use('/api/v1/portfolios', require('./routes/portfolios'));
 
-const PORT = parseInt(process.env.PORT, 10) || 3001;
-server.listen(PORT, (err) => {
-  if (err) console.error(err);
-  console.log('Server ready on port:', PORT);
-})
+  const PORT = parseInt(process.env.PORT, 10) || 3001;
+  server.listen(PORT, (err) => {
+    if (err) console.error(err);
+    console.log('Server ready on port:', PORT);
+  })
+}
+
+runServer();
+
