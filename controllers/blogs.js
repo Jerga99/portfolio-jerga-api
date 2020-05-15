fd200407216fa3a528c2759088a3a1fbf0f8bce1
Filip Jerga <jerga99@gmail.comm>
2020-05-15 13:23:18 +0200

    18 Change response in create blog in case of error

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -27,7 +27,7 @@ exports.createBlog = async (req, res) => {
     const createdBlog = await blog.save();
     return res.json(createdBlog);
   } catch(e) {
-    return res.status(422).send(e);
+    return res.status(422).send(e.message);
   }
 }
 
