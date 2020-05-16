ae8ae6c22440d08afe8c406c18d7f0979285c44d
Filip Jerga <jerga99@gmail.comm>
2020-05-16 11:25:21 +0200

    20 Slugify Title

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 
 
+const slugify = require('slugify');
 const mongoose = require('mongoose');
 const Blog = mongoose.model('Blog');
 
@@ -46,8 +47,12 @@ exports.updateBlog = async (req, res) => {
       return res.status(422).send(err.message);
     }
 
-    // TODO: Check if user is publishing blog
-    // and if user is publishing then create SLUG
+    if (body.status && body.status === 'published' && !blog.slug) {
+      blog.slug = slugify(blog.title, {
+        replacement: '-',
+        lower: true
+      });
+    }
 
     blog.set(body);
     blog.updateAt = new Date();
